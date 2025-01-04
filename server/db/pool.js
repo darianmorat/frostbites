9a82660617d9022e384c38b6ce6e219bc81634eb
darianmorat <yojhandariantoledomora@gmail.com>
2025-01-04 16:29:37 -0500

    refactor(pool): replace hardcoded values with environment variables

--- server/db/pool.js
+++ server/db/pool.js
@@ -1,12 +1,16 @@
 import pg from "pg";
+import dotenv from 'dotenv'
+
 const { Pool } = pg;
 
+dotenv.config() // Load environment variables from .env file
+
 const pool = new Pool({
-   user: 'darianmorat',
-   password: '2004',
-   database: 'frostbites',
-   host: 'localhost',
-   port: 5432,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
+   host: process.env.DB_HOST,
+   port: process.env.DB_PORT,
 });
 
 export default pool
