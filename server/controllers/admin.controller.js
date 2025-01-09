5917fbae17ccbe565eb18e0380451fe1e533be89
darianmorat <yojhandariantoledomora@gmail.com>
2025-01-09 17:46:24 -0500

    feat(admin): add route and controller for getting admin stats

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -2,25 +2,13 @@ import pool from '../db/pool.js';
 
 export const adminPanel = async (req, res) => {
    try {
-      // const { user_name, user_email } = req.body
-       
-      // await pool.query('UPDATE users SET user_name = $1, user_email = $2 WHERE user_id = $3 RETURNING user_name, user_email', 
-      //    [user_name, user_email, req.user]
-      // )
-      // res.json({success: true})
+      const count = await pool.query('SELECT COUNT(*) FROM users')
+      const countUsers = count.rows[0]
+
+      res.json({success: true, countUsers})
 
    } catch (err) {
       console.error(err.message)
       res.status(500).json({ success: false, message: 'Server error' })
    }
 }
-
-export const getProducts = async (req, res) => {
-   try {
-      const products = await pool.query('SELECT * FROM products')
-      res.status(200).json({ success: true, data: products });
-   } catch (error) {
-      console.log("error in fetching products:", error.message);
-      res.status(500).json({ success: false, message: "Server Error" });
-   }
-};
--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-import authorization from '../middleware/authorization.js';
-import { adminPanel, getProducts } from '../controllers/admin.controller.js';
+import { adminPanel } from '../controllers/admin.controller.js';
 
 const router = Router();
 
-router.get('/', authorization, adminPanel) 
-
-// router.get('/', authorization, getProducts)
-// router.post('/update', authorization, createProduct) 
-// router.put('/update', authorization, updateProduct) 
-// router.delete('/delete', authorization, deleteProduct) 
+router.get('/', adminPanel) 
 
 export default router;
