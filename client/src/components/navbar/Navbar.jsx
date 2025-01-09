1708b70efd048696ab281e2d0d1b4c1b01633796
darianmorat <yojhandariantoledomora@gmail.com>
2025-01-09 13:58:04 -0500

    fix(navbar): update content and logic

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,30 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import './navbar.css';
 import logo from '../../assets/images/logo/logo.svg';
 import { toast } from "react-toastify";
+import { useRef, useState } from "react";
 
 export const Navbar = ({ isAuthenticated, setAuth, isAdmin, setAdmin }) => {
+
+   const [dropMenu, setDropMenu] = useState(false)
+   const menuRef = useRef(null)
+
+   const toggleDropMenu = (e) => {
+      e.stopPropagation()
+      setDropMenu(!dropMenu)
+   }
+
+   const closeOpenMenus = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+         setDropMenu(false)
+      }
+   }
+
+   document.addEventListener('click', closeOpenMenus)
+
+   // LOGOUT
+   const navigate = useNavigate();
 
    const logout = async (e) => {
       e.preventDefault();
@@ -14,18 +34,19 @@ export const Navbar = ({ isAuthenticated, setAuth, isAdmin, setAdmin }) => {
          setAuth(false);
          setAdmin(false);
          toast.success("Logout successfully");
+         navigate('/') // REDIRECT TO THIS ROUTE AFTER LOGOUT
       } catch (err) {
          console.error(err);
       }
-   };
+   }
 
-   const renderNavLinks = () => (
+   const RenderNavLinks = () => (
       <>
          <li><Link to="/">HOME</Link></li>
          <li><Link to="/about">ABOUT US</Link></li>
          <li><Link to="/contact">CONTACT</Link></li>
       </>
-   );
+   )
 
    const AuthenticatedNav = () => (
       <>
@@ -34,11 +55,19 @@ export const Navbar = ({ isAuthenticated, setAuth, isAdmin, setAdmin }) => {
             {isAdmin && <li><Link to="/admin">ADMIN</Link></li>} 
 
             {/* drop menu for profile, settings and logout */}
-            <li><Link to="/profile">USER</Link></li> 
-            <button className='' onClick={logout}>Logout</button>
+            {/* FIX THAT WHEN CLICK DOESN'T CLOSE AUTOMATICALY */}
+            <div>
+               <button className='dropmenu-btn btn' onClick={toggleDropMenu}>MENU</button>
+               {dropMenu && (
+                  <ul className="dropdown-menu" ref={menuRef}>
+                     <li><Link to="/profile" onClick={() => setDropMenu(false)}>PROFILE</Link></li>
+                     <li><Link className='nav-logout-btn' onClick={logout}>LOGOUT</Link></li>
+                  </ul>
+               )}
+            </div>
          </div>
       </>
-   );
+   )
 
    const UnauthenticatedNav = () => (
       <>
@@ -47,7 +76,7 @@ export const Navbar = ({ isAuthenticated, setAuth, isAdmin, setAdmin }) => {
             <li><Link to="/register">REGISTER</Link></li>
          </div>
       </>
-   );
+   )
 
    return (
       <nav className="navbar">
@@ -58,10 +87,10 @@ export const Navbar = ({ isAuthenticated, setAuth, isAdmin, setAdmin }) => {
                      <img src={logo} alt="Frostbites Logo" className="logo" />
                   </Link>
                </div>
-               {renderNavLinks()}
+               {RenderNavLinks()}
             </div>
             {isAuthenticated ? AuthenticatedNav() : UnauthenticatedNav()}
          </div>
       </nav>
-   );
+   )
 }
