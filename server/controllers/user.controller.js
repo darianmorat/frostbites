89891a3a01c31f12b1917aa886b9e634a709aacf
darianmorat <yojhandariantoledomora@gmail.com>
2025-01-09 17:37:00 -0500

    feat(user.controller): add functionality to delete user account

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -7,24 +7,61 @@ export const userInfo = async (req, res) => {
       )
       res.json(user.rows[0])
 
-   } catch (error) {
+   } catch (err) {
       console.error(err.message)
       res.status(500).json({ success: false, message: 'Server error' })
    }
 }
 
-export const userUpdateInfo = async (req, res) => {
+export const updateUserInfo = async (req, res) => {
    try {
-      const { user_name, user_email } = req.body
-       
-      await pool.query('UPDATE users SET user_name = $1, user_email = $2 WHERE user_id = $3 RETURNING user_name, user_email', 
-         [user_name, user_email, req.user]
-      )
-      res.json({success: true})
+      // Destructure the req.body
+      const { user_name, user_email } = req.body;
 
-   } catch (error) {
-      console.error(err.message)
-      res.status(500).json({ success: false, message: 'Server error' })
+      // Get the current user information
+      const currentUser = await pool.query('SELECT * FROM users WHERE user_id = $1', [req.user]);
+
+      // If the email is different from the current email, check if it's already in use
+      if (user_email && user_email !== currentUser.rows[0].user_email) {
+         const emailCheck = await pool.query('SELECT * FROM users WHERE user_email = $1', [user_email]);
+
+         if (emailCheck.rows.length !== 0) {
+            return res.status(409).json({ success: false, message: 'Email already in use' });
+         }
+      }
+
+      // If the user is just changing the username
+      if (user_name) {
+         await pool.query('UPDATE users SET user_name = $1 WHERE user_id = $2 RETURNING user_name', 
+            [user_name, req.user]
+         );
+      }
+
+      // If the user is changing both username and email
+      if (user_name && user_email) {
+         await pool.query('UPDATE users SET user_name = $1, user_email = $2 WHERE user_id = $3 RETURNING user_name, user_email', 
+            [user_name, user_email, req.user]
+         );
+      }
+
+      // Return success response
+      res.json({ success: true, message: 'Profile updated successfully' });
+
+   } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ success: false, message: 'Server error' });
    }
-}
+};
 
+export const deleteUserInfo = async (req, res) => {
+   try {
+
+      await pool.query('DELETE FROM users WHERE user_id = $1', [req.user]);
+
+      res.json({ success: true, message: 'Profile deleted successfully' });
+
+   } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ success: false, message: 'Server error' });
+   }
+};
