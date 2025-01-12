2c28fe58bd1cbe29c65447312898e75de38b2ada
darianmorat <yojhandariantoledomora@gmail.com>
2025-01-12 18:30:24 -0500

    feat(index): export showpassword function

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -2,3 +2,5 @@ export { Navbar } from "./navbar/Navbar";
 export { Footer } from "./footer/Footer";
 export { Location } from "./location/Location";
 export { Cart } from "./cart/Cart";
+
+export { ShowPassword } from "./showPassword/ShowPassword";
