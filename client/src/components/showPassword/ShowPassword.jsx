8c1e6233a7ff88c1148f2334257a1d70d53292d3
darianmorat <yojhandariantoledomora@gmail.com>
2025-01-12 18:29:57 -0500

    feat(showpass): create component for showing input passwords

---
+++ client/src/components/showPassword/ShowPassword.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+
+export const ShowPassword = ({showPassword, setShowPassword}) => { 
+
+   const toggleShowPassword = () => {
+      setShowPassword(prevState => !prevState)
+   }
+
+   return (
+      <>
+         <button type="button" className='btn show-password' onClick={toggleShowPassword}>
+            {showPassword 
+               ? <i className='bx bx-show bx-sm' id="show-icon"></i> 
+               : <i className='bx bx-hide bx-sm' id="show-icon"></i>
+            }
+         </button>
+      </>
+   )
+}
