cb8e54a1b60ed1726a078f093776dbf1726579f2
darianmorat <yojhandariantoledomora@gmail.com>
2025-01-12 18:33:35 -0500

    fix(auth): remove validinfo middleware

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
-import validInfo from '../middleware/validInfo.js';
 import authorization from '../middleware/authorization.js'
 
 import { registerUser, loginUser, verifyUser } from '../controllers/auth.controller.js';
 
 const router = Router();
 
-router.post('/register', validInfo, registerUser)
-router.post('/login', validInfo, loginUser)
+router.post('/register', registerUser)
+router.post('/login', loginUser)
 router.get('/verify', authorization, verifyUser)
 
 export default router;
