ef1b6933b6e0ed992b17a2e69a6ca92571c8daf1
darianmorat <yojhandariantoledomora@gmail.com>
2025-01-13 22:04:39 -0500

    fix(create): add formik to create product form

--- client/src/pages/public/shop/CreateProduct.jsx
+++ client/src/pages/public/shop/CreateProduct.jsx
@@ -1,51 +1,50 @@
 /* eslint-disable react/prop-types */
 
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { useState } from 'react';
 import { RemoveScroll } from 'react-remove-scroll';
 import { toast } from 'react-toastify'
 import api from '../../../../api/axios';
 
 export const CreateProductC = ({ isAdmin, addProduct }) => {
-   // ==============
-   // CREATE PRODUCT 
-   // ==============
-   const [name, setName] = useState('')
-   const [image, setImage] = useState('')
-   const [price, setPrice] = useState('')
-
    const [createPopup, setCreatePopup] = useState(false);
 
-   const createProduct = async () => {
-      try {
-         const body = {
-            image: image, 
-            name: name,
-            price: price
+   const formik = useFormik({
+      initialValues: {
+         imageUrl: "",
+         name: "",
+         price: ""
+      },
+      validationSchema: Yup.object({
+         imageUrl: Yup.string().required("Required"),
+         name: Yup.string().required("Required"),
+         price: Yup.string().required("Required")
+      }),
+      onSubmit: async (values) => {
+         try {
+            const res = await api.post('/product/create', values)
+            const data = res.data;
+
+            if (data.success) {
+               setCreatePopup(false)
+               addProduct(data.product);
+               toast.success(data.message);
+            } 
+
+         } catch (err) {
+            console.log(err)
+            toast.error(err.response)
          }
-
-         const res = await api.post('/product/create', body)
-         const data = res.data
-
-         if (data.success) {
-            setName('')
-            setImage('')
-            setPrice('')
-            setCreatePopup(false)
-
-            addProduct(data.product);
-            toast.success(data.message);
-
-         } else {
-            toast.error(data.message);
-         }
-
-      } catch (err) {
-         console.error(err);
       }
-   }
+   })
 
    const createProductPopup = (bool) => {
       setCreatePopup(bool);
+
+      if (!bool) {
+         formik.resetForm()
+      }
    };
 
    return (
@@ -65,27 +64,67 @@ export const CreateProductC = ({ isAdmin, addProduct }) => {
                <RemoveScroll>
                   <div className="popup-content" onClick={(e) => e.stopPropagation()}>
                      <h3>Create Product</h3>
-                     <input 
-                        type="text" 
-                        placeholder="Product Image URL" 
-                        value={image}
-                        onChange={(e) => setImage(e.target.value)} 
-                     />
-                     <input 
-                        type="text" 
-                        placeholder="Product Name" 
-                        value={name}
-                        onChange={(e) => setName(e.target.value)} 
-                     />
-                     <input 
-                        type="number" 
-                        placeholder="Product Price" 
-                        value={price}
-                        onChange={(e) => setPrice(e.target.value)} 
-                     />
-
-                     <button className='btn secondary-btn' onClick={createProduct}>Create</button>
                      <button className='btn close-btn' onClick={()=> createProductPopup(false)}>&#10006;</button>
+                     <form className='form' onSubmit={formik.handleSubmit}>
+                        <label 
+                           htmlFor="imageUrl"
+                           className={`${ formik.touched.imageUrl && formik.errors.imageUrl ? "label-error" : "" }`} 
+                        >
+                           {formik.touched.imageUrl && formik.errors.imageUrl ? formik.errors.imageUrl : "Image-url:"}
+                        </label>
+                        <input 
+                           type="text" 
+                           className={`input ${ formik.touched.imageUrl && formik.errors.imageUrl ? "input-error" : "" }`}
+                           onChange={formik.handleChange}
+                           onBlur={formik.handleBlur} 
+                           value={formik.values.imageUrl}
+                           name="imageUrl"
+                           id="imageUrl"
+                        />
+                        <label 
+                           htmlFor="name"
+                           className={`${ formik.touched.name && formik.errors.name ? "label-error" : ""}`}
+                        >
+                           {formik.touched.name && formik.errors.name ? formik.errors.name : "Name:"}
+                        </label>
+                        <input 
+                           type="text" 
+                           className={`input ${ formik.touched.name && formik.errors.name ? "input-error" : "" }`}
+                           onChange={formik.handleChange}
+                           onBlur={formik.handleBlur} 
+                           value={formik.values.name}
+                           name="name"
+                           id="name"
+                        />
+                        <label 
+                           htmlFor="price"
+                           className={`${ formik.touched.price && formik.errors.price ? "label-error" : ""}`}
+                        >
+                           {formik.touched.price && formik.errors.price ? formik.errors.price : "Price:"}
+                        </label>
+                        <input 
+                           type="text" 
+                           className={`input ${ formik.touched.price && formik.errors.price ? "input-error" : "" }`}
+                           onChange={formik.handleChange}
+                           onBlur={formik.handleBlur} 
+                           value={formik.values.price}
+                           name="price"
+                           id="price"
+                        />
+                        {/* later add functionality for this one */}
+                        <label htmlFor="text">Quantity:</label>
+                        <input 
+                           type="text" 
+                           value="" 
+                           disabled
+                        />
+                        <button 
+                           className='btn secondary-btn' 
+                           disabled={!formik.values.imageUrl || !formik.values.name || !formik.values.price}
+                        >
+                           Create
+                        </button>
+                     </form>
                   </div>
                </RemoveScroll>
             </div>
