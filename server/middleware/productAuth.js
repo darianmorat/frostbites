15a8c4a632d4890f09193247a03346a2622b8c87
darianmorat <yojhandariantoledomora@gmail.com>
2025-01-14 20:26:42 -0500

    feat(product): add middleware for crud operations in products

---
+++ server/middleware/productAuth.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+ 
+dotenv.config() // Load environment variables from .env file
+
+export default async (req, res, next) => {
+   try {
+      const token = req.header('token')
+
+      if (!token) {
+         return res.status(403).json({ success: false, message: 'Not authorized' })
+      }
+
+      const verify = jwt.verify(token, process.env.JWT_SECRET)
+
+      req.user = {
+         userId: verify.user,
+         isAdmin: verify.admin
+      }
+
+      if(req.user.isAdmin === false){
+         return res.status(403).json({ success: false, message: 'Access denied. Admins only' })
+      }
+
+      next()
+
+   } catch (err) {
+      console.error(err.message)
+      res.status(403).json({ success: false, message: 'Not authorized' })
+   }
+}
--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { getProducts, createProduct, updateProduct, deleteProduct } from '../controllers/product.controller.js';
+import productAuth from '../middleware/productAuth.js';
 
 const router = Router();
 
 router.get('/', getProducts)
-router.put('/update/:id', updateProduct) 
-router.post('/create', createProduct) 
-router.delete('/delete/:id', deleteProduct) 
+router.put('/update/:id', productAuth, updateProduct) 
+router.post('/create', productAuth, createProduct) 
+router.delete('/delete/:id', productAuth, deleteProduct) 
 
 export default router;
