d8cd82ca6aabbfaec25211490121a8badb29c2da
darianmorat <yojhandariantoledomora@gmail.com>
2025-01-22 10:44:47 -0500

    feat(admin): add products counter query

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -2,10 +2,13 @@ import pool from '../db/pool.js';
 
 export const adminPanel = async (req, res) => {
    try {
-      const count = await pool.query('SELECT COUNT(*) FROM users');
-      const countUsers = count.rows[0];
+      const resultUsers = await pool.query('SELECT COUNT(*) FROM users');
+      const countUsers = resultUsers.rows[0];
 
-      res.json({ success: true, countUsers });
+      const resultProducts = await pool.query('SELECT COUNT(*) FROM products');
+      const countProducts = resultProducts.rows[0];
+
+      res.json({ success: true, countUsers, countProducts });
    } catch (err) {
       res.status(500).json({ success: false, message: 'Server error' });
    }
