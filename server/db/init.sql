4e240714d548fd3f87500e7022497999f3e7f145
darianmorat <yojhandariantoledomora@gmail.com>
2025-01-30 16:25:29 -0500

    fix(init): update tables

--- server/db/init.sql
+++ server/db/init.sql
@@ -3,38 +3,38 @@ CREATE DATABASE frostbites;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
-   user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+   user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
    user_name VARCHAR(100) NOT NULL,
    user_email VARCHAR(255) NOT NULL,
    user_password VARCHAR(255) NOT NULL,
    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
    is_verified BOOLEAN DEFAULT FALSE,
-   cart_items INT DEFAULT 0,
+   -- cart_items INT DEFAULT 0,
    user_bio VARCHAR(255) NOT NULL DEFAULT 'No bio provided';
 );
 
 CREATE TABLE roles (
-   role_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+   role_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
    role_name VARCHAR(50) UNIQUE NOT NULL
 );
 
 CREATE TABLE user_roles (
-   user_id uuid REFERENCES users(user_id) ON DELETE CASCADE,
-   role_id uuid REFERENCES roles(role_id) ON DELETE CASCADE,
+   user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+   role_id UUID REFERENCES roles(role_id) ON DELETE CASCADE,
    PRIMARY KEY (user_id, role_id)
 );
 
 CREATE TABLE products (
-   product_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+   product_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
    product_img VARCHAR(100) NOT NULL,
    product_name VARCHAR(100) NOT NULL,
    product_price DECIMAL(10, 2) NOT NULL
 );
 
 CREATE TABLE orders (
-   order_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-   user_id uuid REFERENCES users(user_id) ON DELETE CASCADE,
-   product_id uuid REFERENCES products(product_id) ON DELETE CASCADE,   
+   order_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+   user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+   product_id UUID REFERENCES products(product_id) ON DELETE CASCADE,   
    product_name VARCHAR(100),
    product_price DECIMAL(10, 2),
    quantity INT DEFAULT 0,
